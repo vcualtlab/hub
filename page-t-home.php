564570d73663eb481da502b075b5fc7eeaffe359
Mark Luetke <luetkemj@gmail.com>
2015-08-20 11:38:50 -0400

    home page roughed in

--- page-t-home.php
+++ page-t-home.php
@@ -59,7 +59,29 @@ get_header(); ?>
 							<ul>
 								<li>
 									<header>
-										<h2>Social Structure in our Food System</h2>
+										<h2><a href="">Social Structure in our Food System</a></h2>
+										<p>Written by <a href="">kaivalya77</a> on July 17, 2015</p>
+										<i class="fa fa-rss"></i>
+									</header>
+									<div class="content">
+										It won’t completely change the way you see food, but it will definitely change the way you see fast food meat and unnatural meat.
+									</div>
+								</li>
+
+								<li>
+									<header>
+										<h2>David R. Croteau <a href="">@DavidRCroteau</a></h2>
+										<p>July 17, 2015</p>
+										<i class="fa fa-twitter"></i>
+									</header>
+									<div class="content">
+										Structural Barriers Discourage Millennial Couples from Being Egalitarian After Having Kids <a href="">#vcusocy101</a> <a href="">http://nyti.ms/1KBmUr1</a>
+									</div>
+								</li>
+
+								<li>
+									<header>
+										<h2><a href="">Social Structure in our Food System</a></h2>
 										<p>Written by <a href="">kaivalya77</a> on July 17, 2015</p>
 										<i class="fa fa-rss"></i>
 									</header>
